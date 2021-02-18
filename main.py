e479b778140dbaff2518f9ebb29b36ffc805d4b9
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2021-02-18 16:21:24 -0600

    Make single database connection for upload.

--- main.py
+++ main.py
@@ -8,43 +8,49 @@ def convertToBinaryData(filename):
         binaryData = file.read()
     return binaryData
 
-def insertBLOB(photo, table):
+def insertBLOB(dbConnection, photo, table):
     print("Inserting " + photo + " into database...")
     try:
-        connection = mysql.connector.connect(host='192.168.1.2',
-                                             database='catpalooza',
-                                             user='catpalooza',
-                                             password='catpalooza')
-
-        cursor = connection.cursor()
+        databaseCursor = dbConnection.cursor()
         sql_insert_blob_query = "INSERT INTO {} (name, photo, size) VALUES (%s, %s, %s)".format(table)
 
         catPic = convertToBinaryData(photo)
 
         # Convert data into tuple format
         insert_blob_tuple = (os.path.basename(photo), catPic, os.path.getsize(photo))
-        result = cursor.execute(sql_insert_blob_query, insert_blob_tuple)
-        connection.commit()
+        databaseCursor.execute(sql_insert_blob_query, insert_blob_tuple)
+        dbConnection.commit()
         print("Image successfully entered into table {}".format(table))
 
     except Error as error:
         print("Failed inserting BLOB into MySQL table {}".format(error))
 
     finally:
-        if (connection.is_connected()):
-            cursor.close()
-            connection.close()
-            print("MySQL connection is closed")
+        databaseCursor.close()
 
-def uploadDirectoryToDatabase(directory, table):
+def uploadDirectoryToDatabase(dbConnection, directory, table):
     for file in os.listdir(directory):
         if os.path.isdir(os.path.join(directory, file)):
-            uploadDirectoryToDatabase(os.path.join(directory, file), table)
+            uploadDirectoryToDatabase(dbConnection, os.path.join(directory, file), table)
         elif file.endswith('.jpg') or file.endswith('.jpeg') or file.endswith('.png') or file.endswith('.webp'):
-            insertBLOB(os.path.join(directory, file), table)
+            insertBLOB(dbConnection, os.path.join(directory, file), table)
+
+def openDatabaseConnection():
+    connection = mysql.connector.connect(host='192.168.1.2',
+                                             database='catpalooza',
+                                             user='catpalooza',
+                                             password='catpalooza')
+
+    return connection
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', type=str, default='.', help='The directory containing images to upload.')
     args = parser.parse_args()
-    uploadDirectoryToDatabase(str(args.dir), 'photos')
+
+    dbConnection = openDatabaseConnection()
+    print("Opened connection to mysql database")
+    uploadDirectoryToDatabase(dbConnection, str(args.dir), 'photos')
+    if (dbConnection.is_connected()):
+        dbConnection.close()
+        print("MySQL connection is closed")
